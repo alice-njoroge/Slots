82672d99cff4c35e8e6e48b55c8680cfc7369e4b
Alice Wanjiku <alicenjoroge707@gmail.com>
2024-08-20 19:10:53 +0200

    Vue Slots - modal that can have the parents pass any templates

---
+++ src/AppModal.vue
@@ -0,0 +1,20 @@
+<script setup>
+import {XMarkIcon} from "@heroicons/vue/20/solid";
+
+defineProps({
+  title: {type: String, default: 'Modal Title'}
+});
+</script>
+
+<template>
+  <div class="modal-wrapper">
+
+    <div class="modal-wrapper-inner">
+      <div class="flex text-right">
+        <button class="h-5 w-4 top-0 right-0 inline-block"  @click="$emit('close')"> <XMarkIcon /> </button>
+      </div>
+      <slot name="title">{{ title }}</slot>
+      <slot name="body"></slot>
+    </div>
+  </div>
+</template>
